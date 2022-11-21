11c0d6b352afa202dd694494ca44b6f57a206d92
Ammar-Alhamwi <96533950+Ammar-Alhamwi@users.noreply.github.com>
2022-11-21 19:25:17 +0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     /**
      * Create a new controller instance.
-     *cdjdsoicjdsoij
+     *welcom to syria
      * @return void
      */
     public function __construct()
