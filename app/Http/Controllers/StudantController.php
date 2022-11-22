fd4342c29de9b808d0583d137702000d0f848bbf
Ammar ALhamwi <ammaralhamwi98@gmail.com>
2022-11-22 17:40:48 +0300

    edit

--- app/Http/Controllers/StudantController.php
+++ app/Http/Controllers/StudantController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\studant;
 use Illuminate\Http\Request;
-use App\Models\Student;
+
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\studentinfo;
