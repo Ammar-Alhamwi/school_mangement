61c6f0787f77951eacae85ccc7c102efe4d18c2e
unknown <ammaralhamwi98@gmail.com>
2023-03-24 17:13:35 +0300

    mff

--- resources/views/marks/marks.blade.php
+++ resources/views/marks/marks.blade.php
@@ -14,7 +14,7 @@
     @foreach ($sesone1 as $item)
 
         <li >
-
+            {{-- hello this test --}}
             <div class="icon"><i class="fa-solid fa-bicycle"></i></div>
             <div class="title">{{$item->name_sub}}</div></div>
             <div class="descr" style="font-size: 30px; color: #86fde8">{{$item->value}}</div>
